0328b112d34f9959a977e4502de630cdf9f5b92b
Soham Thaker <sohamthaker@Sohams-MacBook-Pro.local>
2022-12-21 13:09:29 -0500

    added some files

---
+++ BestTimeToBuyAndSellStock.js
@@ -0,0 +1,12 @@
+var maxProfit = function(prices) {
+    let buyingStockPrice = Number.MAX_SAFE_INTEGER
+    let sellingStockPrice = 0
+
+    for(let i = 0; i < prices.length; i++) {
+        if(prices[i] < buyingStockPrice)
+            buyingStockPrice = prices[i]
+        else if(prices[i] - buyingStockPrice > sellingStockPrice)
+            sellingStockPrice = prices[i] - buyingStockPrice
+    }
+    return sellingStockPrice
+};
---
+++ ContainsDuplicate.js
@@ -0,0 +1,13 @@
+var containsDuplicate = function(nums) {
+    let dup = new Set()
+    for(let i = 0; i < nums.length; i++) {
+        if(dup.has(nums[i])) {
+            return true
+        }
+        else {
+            dup.add(nums[i])
+        }
+    }
+    return false
+};
+
